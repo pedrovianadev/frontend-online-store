9a112f576a16af38769f82abe64176c4e40e4e1b
Pedro Viana <pedroviana.developer13@gmail.com>
2023-01-11 16:56:26 -0300

    requisito 3

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import './App.css';
+import { Switch, Route } from 'react-router-dom';
 import Home from './Pages/Home';
+import Cart from './Pages/Cart';
 
 function App() {
   return (
     <div className="App">
-      <Home />
+      <Switch>
+        <Route exact path="/" component={ Home } />
+        <Route exact path="/Cart" component={ Cart } />
+      </Switch>
     </div>
   );
 }
