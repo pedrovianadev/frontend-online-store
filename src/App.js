e2011fb01449503d9762ff3be8e133cfeadcb6e3
Pedro Viana <pedroviana.developer13@gmail.com>
2023-01-11 19:58:30 -0300

    requisito 5

--- src/App.js
+++ src/App.js
@@ -1,20 +1,26 @@
 import React from 'react';
 import './App.css';
-import { Switch, Route } from 'react-router-dom';
+import { Switch, Route, BrowserRouter } from 'react-router-dom';
 import Home from './Pages/Home';
 import Cart from './Pages/Cart';
 import Category from './Components/Category';
+import NotFound from './Components/NotFound';
 
-function App() {
-  return (
-    <div className="App">
-      <Switch>
-        <Route exact path="/" component={ Home } />
-        <Route exact path="/Cart" component={ Cart } />
-      </Switch>
-      <Category />
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <BrowserRouter>
+          <Switch>
+            <Route exact path="/" component={ Home } />
+            <Route exact path="/Cart" component={ Cart } />
+            <Route path="*" component={ NotFound } />
+          </Switch>
+        </BrowserRouter>
+        <Category />
+      </div>
+    );
+  }
 }
 
 export default App;
