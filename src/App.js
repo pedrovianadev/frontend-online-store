25fa9cde045b8ad1673c0b5c67f0be8fa872fcd4
Pedro Viana <pedroviana.developer13@gmail.com>
2023-01-11 16:06:03 -0300

    requisito 2

--- src/App.js
+++ src/App.js
@@ -1,22 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Home from './Pages/Home';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={ logo } className="App-logo" alt="logo" />
-        <p>Edit src/App.js and save to reload.</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learning React
-        </a>
-      </header>
+      <Home />
     </div>
   );
 }
