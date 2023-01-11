f716a3835de417aadd92a0bf1e238397d9cf7499
Pedro Viana <pedroviana.developer13@gmail.com>
2023-01-11 17:20:28 -0300

    requisito 4

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import { Switch, Route } from 'react-router-dom';
 import Home from './Pages/Home';
 import Cart from './Pages/Cart';
+import Category from './Components/Category';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Route exact path="/" component={ Home } />
         <Route exact path="/Cart" component={ Cart } />
       </Switch>
+      <Category />
     </div>
   );
 }
---
+++ src/Components/Category.jsx
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+import { getCategories } from '../services/api';
+
+class Category extends Component {
+  state = {
+    categories: [],
+  };
+
+  componentDidMount() {
+    this.handleCategories();
+  }
+
+  handleCategories = async () => {
+    const fetch = await getCategories();
+    this.setState({
+      categories: fetch,
+    });
+    return fetch;
+  };
+
+  render() {
+    const { categories } = this.state;
+    return (
+      <div>
+        {categories.map(({ id, name }) => (
+          <button
+            data-testid="category"
+            type="button"
+            key={ id }
+          >
+            { name }
+          </button>
+        ))}
+      </div>
+    );
+  }
+}
+
+export default Category;
