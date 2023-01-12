30c8188483621c122a6c2257965ef622d9e4158c
matheus-amsfer <matheus.cid47@gmail.com>
2023-01-12 18:18:15 -0300

    requisito 6

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
 import React from 'react';
 import './App.css';
-import { Switch, Route, BrowserRouter } from 'react-router-dom';
+import { Switch, Route } from 'react-router-dom';
 import Home from './Pages/Home';
 import Cart from './Pages/Cart';
-import Category from './Components/Category';
 import NotFound from './Components/NotFound';
 
 class App extends React.Component {
   render() {
     return (
       <div className="App">
-        <BrowserRouter>
-          <Switch>
-            <Route exact path="/" component={ Home } />
-            <Route exact path="/Cart" component={ Cart } />
-            <Route path="*" component={ NotFound } />
-          </Switch>
-        </BrowserRouter>
-        <Category />
+        <Switch>
+          <Route exact path="/" component={ Home } />
+          <Route exact path="/Cart" component={ Cart } />
+          <Route path="*" component={ NotFound } />
+        </Switch>
       </div>
     );
   }
